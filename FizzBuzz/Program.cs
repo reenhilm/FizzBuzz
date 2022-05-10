028f7a246a93c25caf9f667979e416d809947e7c
35844 <35844@DESKTOP-8DL5ISS>
2022-05-10 14:48:02 +0200

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,28 @@
+﻿int[] arrXYZ = new int[3];
+string[] sXYZ = Console.ReadLine().Split(' ',3);
+arrXYZ[0] = int.Parse(sXYZ[0]);
+arrXYZ[1] = int.Parse(sXYZ[1]);
+arrXYZ[2] = int.Parse(sXYZ[2]) + 1;
+
+static bool isDevicable(int a, int b)
+{
+    return a % b == 0;
+}
+const string FIZZ = "Fizz";
+const string BUZZ = "Buzz";
+int iLowestDenominator = arrXYZ[0] * arrXYZ[1];
+
+for (int i = 1; i < arrXYZ[2]; i++)
+{
+    //Smallest Denominator of X and Y
+    if (isDevicable(i, iLowestDenominator))
+        Console.WriteLine(string.Concat(FIZZ, BUZZ));
+    //X
+    else if (isDevicable(i, arrXYZ[0]))
+        Console.WriteLine(FIZZ);
+    //Y
+    else if (isDevicable(i, arrXYZ[1]))
+        Console.WriteLine(BUZZ);
+    else
+        Console.WriteLine(i);
+}
